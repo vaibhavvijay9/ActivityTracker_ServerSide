5d89c885c729bdc8db8ca71f676f8c80e23f57fc
vaibhavvijay9 <vaibhavvijay9@gmail.com>
2019-07-13 15:58:12 +0530

    undo changes

--- src/main/java/com/activitytracker/api/JerseyApplication.java
+++
@@ -1,9 +0,0 @@
-package com.activitytracker.api;
-
-import org.glassfish.jersey.server.ResourceConfig;
-
-public class JerseyApplication extends ResourceConfig {
-    public JerseyApplication() {
-        packages("com.activitytracker.api");
-    }
-}
